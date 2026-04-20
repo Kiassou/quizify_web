350873b27e3498de7c77c7717c3256fdf1599851
Kiassou <gaoussouthiero04@gmail.com>
2026-04-20 01:19:38 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { routes } from './app/app.routes';
 import { App } from './app/app';
+import { provideRouter, withHashLocation } from '@angular/router';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes, withHashLocation())
+  ]
+});
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
