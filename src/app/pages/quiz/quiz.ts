4e567b8237b96f1fd0b5a40165c23da0957f9ec4
Kiassou <gaoussouthiero04@gmail.com>
2026-04-29 17:52:55 +0000

    home-player v6

--- src/app/pages/quiz/quiz.ts
+++ src/app/pages/quiz/quiz.ts
@@ -1,10 +1,9 @@
-// quiz.component.ts
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-quiz', // Assure-toi que le sélecteur correspond à ta route
+  selector: 'app-quiz', 
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './quiz.html',
@@ -15,7 +14,7 @@ export class QuizComponent implements OnInit {
 
   categories = [
     { id: 'sciences-quiz', name: 'Sciences', icon: 'bi-lightbulb-fill', count: 10, color: '#e73c7e' },
-    { id: 'histoire-quiz', name: 'Histoire', icon: 'bi-bank', count: 10, color: '#23a6d5' },
+    { id: 'histoires-quiz', name: 'Histoire', icon: 'bi-bank', count: 10, color: '#23a6d5' },
     { id: 'tech-quiz', name: 'Tech', icon: 'bi-cpu-fill', count: 10, color: '#23d5ab' },
     { id: 'sport-quiz', name: 'Sport', icon: 'bi-trophy-fill', count: 10, color: '#ee7752' }
   ];
@@ -27,9 +26,7 @@ export class QuizComponent implements OnInit {
     if (storedName) this.userName = storedName;
   }
 
-  // Fonction pour rediriger vers le quiz spécifique
   startQuiz(categoryId: string) {
-    // Redirige vers /quiz/sciences, /quiz/tech, etc.
-    this.router.navigate(['/quiz', categoryId]);
+    this.router.navigate([categoryId]);
   }
 }
